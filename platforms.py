de9abf5da4a8fef258cdc352cd228b11b0cfeb24
braek1018 <bdk6524@mavs.uta.edu>
2024-10-13 02:53:41 -0500

    NSBE-HackUTA-6-

--- platforms.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Platform:
-    def __init__(self, x, y, width, height, color=(0, 255, 0)):  # Green color
-        self.rect = pygame.Rect(x, y, width, height)
-        self.color = color
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
